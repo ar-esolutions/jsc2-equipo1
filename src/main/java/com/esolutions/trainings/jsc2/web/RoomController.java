16703193d49079aff20cba084fff14e601c7ca37
nahuelbaldassari <citroenc3!>
2018-10-08 18:46:05 -0300

    Metodo busqueda tipo de rooms

--- src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
+++ src/main/java/com/esolutions/trainings/jsc2/web/RoomController.java
@@ -1,14 +1,26 @@
 package com.esolutions.trainings.jsc2.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.esolutions.trainings.jsc2.logic.RoomRepeatedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RoomController {
+    private final RoomRepeatedService service;
 
+    @Autowired
+    public RoomController(RoomRepeatedService service) {
+        this.service = service;
+    }
+/*
     @GetMapping(value = "/floors/{floor}/rooms/{room}")
     public GuestResponse getGuestNumber(@PathVariable int floor, @PathVariable int room){
         return new GuestResponse(null);
     }
+*/
+    @RequestMapping(method = RequestMethod.POST, value = "/floors/{floor}/rooms/{room}/book")
+    public void getRoomAndFloor(@PathVariable int floor, @PathVariable int room){
+         this.service.busquedaRooms(floor, room);
+    }
+
 }
