79186f0cfd2c7b838d63daf8b9df5b705591fdec
nahuelbaldassari <citroenc3!>
2018-10-12 19:54:03 -0300

    Metodo priv req 3. Pasa int a binario y lo guarda en un string

--- src/main/java/com/esolutions/trainings/jsc2/logic/GuestRepeatedService.java
+++ src/main/java/com/esolutions/trainings/jsc2/logic/GuestRepeatedService.java
@@ -26,7 +26,7 @@ public class GuestRepeatedService {
 	}
 
 
-	public String parseBinaryFloorAndRoom(int floor, int room){
+	private String parseBinaryFloorAndRoom(int floor, int room){
 
 		String binaryFloor = Integer.toBinaryString(floor);  // Paso el int floor a binario y lo guardo en string
 		String binaryRoom = Integer.toBinaryString(room);    // Paso el int room a binario y lo guardo en string
