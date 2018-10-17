f07cb183272b629590f47554e50a53f18504f2f5
NicoPacha <gokxi14@gmail.com>
2018-10-17 20:42:21 -0300

    SonarLint Cambios

--- src/main/java/com/esolutions/trainings/jsc2/util/javaUtils.java
+++ src/main/java/com/esolutions/trainings/jsc2/util/javaUtils.java
@@ -1,18 +1,10 @@
 package com.esolutions.trainings.jsc2.util;
 
-public class javaUtils {
-
-    public javaUtils() {
+public class JavaUtils {
+    private JavaUtils() {
     }
 
     public static boolean isPerfectSquare(long n) {
-        /*
-        double n1= Math.sqrt(n);
-        double n2= Math.sqrt(n);
-        System.out.println("n1 : "+ n1);
-        System.out.println("n2 : "+ n2);
-        System.out.println(n1*n2);
-        */
-        return ((Math.sqrt(n)) * (Math.sqrt(n)) == n);
+        return ((int)(Math.sqrt(n)) * (int)(Math.sqrt(n)) == n);
     }
 }
